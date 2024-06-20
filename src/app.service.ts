399e036477815876343b3a11f7835fdfd31852c5
s.gadekar <sgadekar@calibo.com>
2024-06-20 09:26:16 +0530

    page title added and same change updated in test case

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,14 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello, Welcome to Engineering Lab! Start editing to see some magic happen :)';
-  }
+    const pageTitle = 'NestJS 10';
+    const pageContent = 'Hello, Welcome to Engineering Lab! Start editing to see some magic happen :)';
+   return `<html>
+        <head>
+          <title>${pageTitle}</title>
+        </head>
+        <body>
+          <p>${pageContent}</p>
+        </body>
+      </html>`;  }
 }
